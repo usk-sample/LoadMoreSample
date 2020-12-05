68527a3f2b473599c2892b0cbc3d122e9c4a4f6b
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-12-05 11:56:57 +0900

    Initial Commit

---
+++ LoadMoreSample/LoadMoreSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadMoreSampleApp.swift
+//  LoadMoreSample
+//
+//  Created by Yusuke Hasegawa on 2020/12/05.
+//
+
+import SwiftUI
+
+@main
+struct LoadMoreSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
